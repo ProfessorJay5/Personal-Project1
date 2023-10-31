3f107f268a75c88d29357b93a26a80b33c814259
Jayden <jaydengrussell@gmail.com>
2023-10-31 01:30:07 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let j = document.getElementById("j");
+let a = document.getElementById("a");
+let y = document.getElementById("y");
+let d = document.getElementById("d");
+let e = document.getElementById("e");
+let n = document.getElementById("n");
+let r = document.getElementById("r");
+let u = document.getElementById("u");
+let s = document.getElementById("s");
+let s1 = document.getElementById("s1");
+let e1 = document.getElementById("e1");
+let l = document.getElementById("l");
+let l1 = document.getElementById("l1");
+
+function changeJ()
+{
+    j.innerText = "J";
+}
+function changeA()
+{
+    a.innerText = "a";
+}
+function changeY()
+{
+    y.innerText = "y";
+}
+function changeD()
+{
+    d.innerText = "d";
+}
+function changeE()
+{
+    e.innerText = "e";
+}
+function changeN()
+{
+    n.innerText = "n";
+}
+function changeR()
+{
+    r.innerText = "R";
+}
+function changeU()
+{
+    u.innerText = "u";
+}
+function changeS()
+{
+    s.innerText = "s";
+}
+function changeS1()
+{
+    s1.innerText = "s";
+}
+function changeE1()
+{
+    e1.innerText = "e";
+}
+function changeL()
+{
+    l.innerText = "l";
+}
+function changeL1()
+{
+    l1.innerText = "l";
+}
+
+
+
+setTimeout(changeJ, 1000);
+setTimeout(changeA, 1500);
+setTimeout(changeY, 2000);
+setTimeout(changeD, 2500);
+setTimeout(changeE, 3000);
+setTimeout(changeN, 3500);
+setTimeout(changeR, 4000);
+setTimeout(changeU, 4500);
+setTimeout(changeS, 5000);
+setTimeout(changeS1, 5500);
+setTimeout(changeE1, 6000);
+setTimeout(changeL, 6500);
+setTimeout(changeL1, 7000);
+
